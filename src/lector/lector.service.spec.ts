cde924a1c057750d080a76ac84959fa75eeea935
Sunaidy <sjauregui@refcfg.cu>
2023-08-24 21:33:43 -0400

    Add document to postman

--- src/lector/lector.service.spec.ts
+++ src/lector/lector.service.spec.ts
@@ -3,15 +3,47 @@ import { LectorService } from './lector.service';
 import { lectorProviders } from './lector.provaiders';
 import { LectorController } from './lector.controller';
 import { faker } from '@faker-js/faker';
+import { DataTypes, Sequelize } from 'sequelize';
 
 describe('LectorService', () => {
   let service: LectorService;
+  let sequelize: Sequelize;
+  let lectorMock
   beforeEach(async () => {
+     sequelize = new Sequelize('bibloteca', 'desarrollo', 'desarrollo*2023', {
+     host: 'localhost',
+     dialect: 'mysql', 
+    });
+
+   const Lector = sequelize.define('Lector', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {});
+    lectorMock=Lector
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LectorController],
       providers: [
         LectorService,
-        ...lectorProviders
+        {
+          provide: 'LECTOR_REPOSITORY',
+          useValue: Lector,
+        },
+        {
+          provide: Lector, 
+          useValue: lectorMock, 
+        },
+        {
+          provide: Sequelize, 
+          useValue: sequelize, 
+        }
       ],
     }).compile();
 
@@ -22,6 +54,14 @@ describe('LectorService', () => {
     expect(service).toBeDefined();
   });
 
+  it('Retortar lector dado id', async () => {
+    lectorMock.findOne = jest.fn().mockResolvedValue({ id: 1, nombre: 'Angel', createdAt:'2023-08-24 20:39:20',updatedAt:'2023-08-24 20:39:20'  });
+
+    const libro = await service.findOne(1);
+
+    expect(libro).toEqual({ id: 1, nombre: 'Angel', createdAt:'2023-08-24 20:39:20',updatedAt:'2023-08-24 20:39:20'  });
+    expect(lectorMock.findOne).toHaveBeenCalledWith({ where: { id: 1 } });
+  });
 });
 
 
